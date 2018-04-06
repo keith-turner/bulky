306ff55fdad71196ce6010777958846847147aba
Keith Turner <kturner@apache.org>
2018-04-06 19:35:20 -0400

    add more test

--- test1.sh
+++ test1.sh
@@ -4,8 +4,12 @@ hadoop fs -rm -R /bulk0*
 accumulo shell -u root -p secret -e 'deletetable -f bulky'
 mvn package
 ./bin/run.sh cmd.Generate IA 100 1000 1000000 /bulk01
-./bin/run.sh cmd.Generate IB 2 1000 2000 /bulk02
+./bin/run.sh cmd.Generate IB 100 1 1000 /bulk02
+./bin/run.sh cmd.Generate IC 10 1 1000 /bulk03
+./bin/run.sh cmd.Generate ID 10 10 10000 /bulk04
 ./bin/run.sh cmd.Split 100
 ./bin/run.sh cmd.Import /bulk01
 ./bin/run.sh cmd.Import /bulk02
+./bin/run.sh cmd.Import /bulk03
+./bin/run.sh cmd.Import /bulk04
 ./bin/run.sh cmd.Verify
