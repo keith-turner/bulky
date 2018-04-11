b44a22aab91c54044b6c49cab2abb2f35d07b411
Keith Turner <kturner@apache.org>
2018-04-11 12:49:00 -0400

    move splits in test

--- test1.sh
+++ test1.sh
@@ -13,11 +13,11 @@ fi
 
 accumulo shell -u root -p secret -e 'deletetable -f bulky'
 mvn package
+./bin/run.sh cmd.Split 100
 ./bin/run.sh cmd.Generate IA 100 10000 1000000 /bulk01
 ./bin/run.sh cmd.Generate IB 100 1 1000 /bulk02
 ./bin/run.sh cmd.Generate IC 10 1 1000 /bulk03
 ./bin/run.sh cmd.Generate ID 10 10 10000 /bulk04
-./bin/run.sh cmd.Split 100
 ./bin/run.sh cmd.Import $BULK_API /bulk01
 ./bin/run.sh cmd.Import $BULK_API /bulk02
 ./bin/run.sh cmd.Import $BULK_API /bulk03
